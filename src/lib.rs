a62cb53299a1e796388fec7647e62e90d18ae3c2
Nathan Hammond <nhammond@microsoft.com>
2025-06-03 14:40:28 +0800

    Demonstrate upstream issue.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,33 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::path::Path;
+
+#[cfg(windows)]
+pub fn bytes2path(b: &[u8]) -> &std::path::Path {
+    use std::str;
+    std::path::Path::new(str::from_utf8(b).unwrap())
+}
+
+pub fn is_dirty(dir: &Path) -> Result<Option<Vec<String>>, git2::Error> {
+    let repo = git2::Repository::discover(dir)?;
+
+    let mut entries = Vec::new();
+    let mut options = git2::StatusOptions::new();
+    options
+        .show(git2::StatusShow::IndexAndWorkdir)
+        .include_untracked(true);
+    let statuses = repo.statuses(Some(&mut options))?;
+    let dirty_tree = !statuses.is_empty();
+    if dirty_tree {
+        for status in statuses.iter() {
+            let path = bytes2path(status.path_bytes());
+            entries.push(format!("{} ({:?})", path.display(), status.status()));
+        }
+    }
+
+    if entries.is_empty() {
+        Ok(None)
+    } else {
+        Ok(Some(entries))
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +35,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn check_upstream() {
+        match is_dirty(Path::new(".")) {
+            Ok(None) => {}
+            Ok(Some(entries)) => {
+                println!("{}", entries.join("\n"));
+            }
+            Err(e) => panic!("Error checking dirty state: {}", e),
+        }
     }
 }
